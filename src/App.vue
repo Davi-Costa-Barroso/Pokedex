d6fad572cbf4160af70f2bb12f34814f076731c2
Davi-Costa-Barroso <daviinformatica228@gmail.com>
2024-03-12 19:29:50 -0300

    Feat: styles pokedex

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="pokedex">
+   <h1 class="text--yellow">Pokédex</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss" scoped>
+#pokedex {
+ display: flex;
+ flex-direction: column;
+ min-height: 100vh;
+ max-width: 100px;
+ margin: 0 auto;
+ padding: 24px;
+}
+</style>
